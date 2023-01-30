aedec12f8cbed93a1c1ad25627b3f9ea5b38b9e9
harsh <harshsoni13112003@gmail.com>
2023-01-30 21:53:36 +0530

    velocity-day-30-01-2023

---
+++ webdev/30-01-2023 -js/js.js
@@ -0,0 +1,14 @@
+function even(value){
+    
+}
+
+/*
+let sum = 0;
+function addval(value){
+    sum+=value;
+    console.log(sum);
+}
+const b = [1,2,34,56,78];
+b.forEach(addval);
+console.log(sum);
+*/
---
+++ webdev/30-01-2023- homework/problem_3.js
@@ -0,0 +1,15 @@
+dict={
+    Lackadaisical: "negligent",
+    indifferent: "half-hearted", 
+    indolent: "Lazy",
+    obstinate: "stubborn",
+    mulish: "head strong, obdurate",
+    stingy: "miserly, penny pincher",
+    frugal: "thrifty, austere",
+    resilient: "to be able to endure/ withstand adversity & recover from worse situations",
+    persistent: "determined, perseverance",
+    blase: "insocient"
+}
+for(i in dict){
+    console.log(i+" is meant by "+dict[i]);
+}
